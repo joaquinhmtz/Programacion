eab546705065b50f3560918a87dc3f15a56465f7
Joaquín Hernández <joaquin_downhill@hotmail.com>
2017-02-01 17:02:51 -0600

    Práctica dos

---
+++ Prácticas/P02/Var_Globales.java
@@ -0,0 +1,27 @@
+public class Var_Globales{
+
+public static byte var_byte;
+public static short var_short;
+public static int var_int;
+public static long var_long;
+public static double var_double;
+public static char var_char;
+public static boolean var_boolean;
+public static String var_string;
+
+	public static void main (String[] args){
+
+
+	System.out.println("Valor de byte: " + var_byte);
+	System.out.println("Valor de short: " +var_short);
+	System.out.println("Valor de int: " +var_int);
+	System.out.println("Valor de long: " +var_long +"L");
+	System.out.println("Valor de double: " +var_double +"d");
+	System.out.println("Valor de char: " +var_char +"\u0000");
+	System.out.println("Valor de boolean: " +var_boolean);
+	System.out.println("Valor de String: " + var_string);
+
+
+
+	}
+}
---
+++ Prácticas/P02/Var_Locales.java
@@ -0,0 +1,20 @@
+public class Var_Locales{
+		public static void main (String[] args){
+
+	byte var_byte = 127;
+	short var_short = 32767;
+	int var_int = 2147483647;
+	long var_long = 9223372036854775807L;
+	char var_char = '2';
+	String var_string = null;
+	String var_string2 = "hola java";
+
+	System.out.println("Valor de byte: " + var_byte);
+	System.out.println("Valor de short: " + var_short);
+	System.out.println("Valor de int: " + var_int);
+	System.out.println("Valor de long: " + var_long);
+	System.out.println("Valor de char: " + var_char);
+	System.out.println("Valor de String: " + var_string);
+	System.out.println("Valor de String: " + var_string2);
+	}
+}
