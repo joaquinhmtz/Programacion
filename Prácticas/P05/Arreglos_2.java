8c0c5b254b1aceaa7261bf9ee94ebc5da9da4c67
Joaquín Hernández <joaquin_downhill@hotmail.com>
2017-02-23 11:40:24 -0600

    Práctica 5

--- Prácticas/P05/Arreglos_2.java
+++ Prácticas/P05/Arreglos_2.java
@@ -16,26 +16,30 @@ public class Arreglos_2 {
 
 		try {
 
-		flujoS.println("¿De qué tamaño es su vector?");
-		dato = flujoE.readLine();
-		tamaño = Integer.parseInt(dato);
+			flujoS.println("¿De qué tamaño es su vector?");
+			dato = flujoE.readLine();
+			tamaño = Integer.parseInt(dato);
 
-		vector = new int [tamaño];
-		Random r = new Random();
+			vector = new int [tamaño];
+			Random r = new Random();
 
-		flujoS.println();
+			flujoS.println();
 
-		for(int i = 0; i < vector.length; i++) {
-			flujoS.println(r.nextInt(300));
-		}
+			for(int i = 0; i < vector.length; i++) {
+				vector[i] = (r.nextInt(300));
+				flujoS.println(vector[i]);
+			}
 
-		flujoS.println("Ingresa número, que será la última cifra, con está sabremos que cantidades tienen este último número: ");
-		dato = flujoE.readLine();
-		cifra = Integer.parseInt(dato);
-		
-		if(cifra >= 0 && cifra <= 9) {
+			flujoS.println("Ingresa número, que será la última cifra, con está sabremos que cantidades tienen este último número: ");				dato = flujoE.readLine();
+			cifra = Integer.parseInt(dato);
 
-		}else flujoS.println("La cifra ingresada no es válida.");
+			if(cifra >= 0 && cifra <= 9) {
+				
+				for(int i = 0; i < vector.length; i++) {
+					if(vector[i]%10 == cifra) {
+					flujoS.println("El número es: "+vector[i]);
+					}							}
+			} else flujoS.println("No ingresaste un dígito válido.");
 
 
 		}catch(IOException e) {}
